646a17f85bfafeb2ee12d122db54a1c34370c07e
huangwyn <91707274+huangwyn@users.noreply.github.com>
2022-08-22 19:18:53 -0400

    lambda work

--- src/main/java/com/quintrix/huang/training/App.java
+++ src/main/java/com/quintrix/huang/training/App.java
@@ -1,5 +1,6 @@
 package com.quintrix.huang.training;
-
+import java.util.HashMap;
+import java.time.LocalTime;
 /**
  * Hello world!
  *
@@ -11,9 +12,17 @@ public class App
         System.out.println( "Hello World!" );
         
         // lets do a lambda here
+        System.out.println("Lets do some lambda!");
         // with a map
-        
-        // a stream here
+        HashMap<String, String> locations = new HashMap<String, String>();
+		locations.put("Thanalan", "Ul'Dah");
+		locations.put("Black Shroud", "Gridania");
+		locations.put("La Noscea", "Limsa Lominsa");
+        locations.forEach((key, value) -> {
+        	System.out.println(value + " is in " + key);
+        });
+		
+		// a stream here
         
         // some fucking with the time space continuum here
     }
